2681e06e28511fa657576b422fbcccf6cf71b0d1
siemens-c2c <joy_mon78@yahoo.co.in>
2020-02-25 11:26:04 +0530

    added extra function

--- day-2/vs_FirstApp/vs_FirstApp/Program.cs
+++ day-2/vs_FirstApp/vs_FirstApp/Program.cs
@@ -13,6 +13,12 @@ namespace vs_FirstApp
         //static int Main(string[] args)
         static void Main()
         {
+            int result = Add(12, 13);
+            Console.WriteLine(result);
+        }
+        static int Add(int firstValue, int secondValue)
+        {
+            return (firstValue + secondValue);
         }
     }
 }
