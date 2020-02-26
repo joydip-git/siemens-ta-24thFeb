e6a57efa7af02115a52ffe39f5eab0487e7e8734
siemens-c2c <joy_mon78@yahoo.co.in>
2020-02-26 12:09:54 +0530

    created enum application

---
+++ day-3/UserDefinedTypes/StructandEnumDemo/Departments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructandEnumDemo
+{
+    //byte,sbyte,short,ushort,int,uint,long,ulong <-- only supported
+    enum Departments : long
+    {
+        HR = 100,
+        TRNG,
+        NotAllocated
+    }
+    enum Operations
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+}
---
+++ day-3/UserDefinedTypes/StructandEnumDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructandEnumDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Departments\n1. HR \n2.Training");
+            Console.Write("Enter choice[h/t]: ");
+            char choice = char.Parse(Console.ReadLine());
+
+            Departments department = Departments.NotAllocated;
+            //Console.WriteLine(department);
+            switch (choice)
+            {
+                case 'h':
+                    department = Departments.HR;
+                    break;
+
+                case 't':
+                    department = Departments.TRNG;
+                    break;
+
+                default:
+                    department = Departments.NotAllocated;
+                    break;
+            }
+
+            //Console.WriteLine($"{name} belongs to {department} = { Convert.ToInt64(department)} department");
+
+
+            switch (department)
+            {
+                case Departments.HR:
+                    Console.WriteLine("welcome to Human Resource department");
+                    break;
+
+                case Departments.TRNG:
+                    Console.WriteLine("welcome to Training department");
+                    break;
+
+                case Departments.NotAllocated:
+                    Console.WriteLine("wait for few days for department allocation");
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("hi...");
+        }
+    }
+}
