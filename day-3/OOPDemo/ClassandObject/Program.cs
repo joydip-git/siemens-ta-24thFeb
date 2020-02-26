5e74277fb42f7131f38977dce9b407f60f1da719
siemens-c2c <joy_mon78@yahoo.co.in>
2020-02-26 16:07:35 +0530

    created OOP app

---
+++ day-3/OOPDemo/ClassandObject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandObject
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Model Name: ");
+                string vehicleModelName = Console.ReadLine();
+
+                Console.Write("Chasis No: ");
+                string vehicleChasisNo = Console.ReadLine();
+
+                Console.Write("Enter purchase Date: ");
+                DateTime purchaseDate = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Price: ");
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                Vehicle maruti800 = new Vehicle();
+                //maruti800 = null;
+                int year = maruti800.CalculateExpiryDate();
+
+
+            }
+
+        }
+    }
+}
---
+++ day-3/OOPDemo/ClassandObject/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandObject
+{
+    class Vehicle
+    {
+        //data member
+        string modelName;
+        string chasisNo;
+        DateTime purchaseDate;
+        decimal price;
+        DateTime expiryDate;
+
+        //functions
+        public int CalculateExpiryDate()
+        {
+            int expiryYear = purchaseDate.Year + 15;
+
+            expiryDate = new DateTime(expiryYear, purchaseDate.Month, purchaseDate.Day);
+            return expiryYear;
+        }
+    }
+}
