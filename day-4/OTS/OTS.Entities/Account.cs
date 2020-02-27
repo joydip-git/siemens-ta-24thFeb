99610dfc02db9b6379228ccf1fed0ea6bad0bf92
siemens-c2c <joy_mon78@yahoo.co.in>
2020-02-27 16:11:05 +0530

    created online transfer app

---
+++ day-4/OTS/OTS.Entities/Account.cs
@@ -0,0 +1,113 @@
+﻿namespace OTS.Entities
+{
+    /// <summary>
+    /// Account class
+    /// </summary>
+    public class Account
+    {
+        #region Data Members
+        private string accountHolderName;
+        private int accountId;
+        private decimal currentBalance;
+        /// <summary>
+        /// fixed minimum balance to be maintained for every account
+        /// </summary>
+        public const decimal MINIMUMBALANCE = 1000;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// gets or sets the current balance
+        /// </summary>
+        public decimal CurrentBalance
+        {
+            get { return currentBalance; }
+            set { currentBalance = value; }
+        }
+
+        /// <summary>
+        /// gets or sets the account id
+        /// </summary>
+        public int AccountId
+        {
+            get { return accountId; }
+            set { accountId = value; }
+        }
+
+        /// <summary>
+        /// gets or sets the name of account holder
+        /// </summary>
+        public string AccountHolderName
+        {
+            get { return accountHolderName; }
+            set { accountHolderName = value; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Account()
+        {
+
+        }
+
+        /// <summary>
+        /// parameterized constructor
+        /// </summary>
+        /// <param name="accountName">name of account holder</param>
+        /// <param name="accountId">id of the account</param>
+        /// <param name="balance">current balance in the account</param>
+        public Account(string accountName, int accountId, decimal balance)
+        {
+            this.accountHolderName = accountName;
+            this.accountId = accountId;
+            this.currentBalance = balance;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Method to credit trnsferred amount into the account
+        /// </summary>
+        /// <param name="amount">
+        /// amount to be credited
+        /// </param>
+        /// <returns>
+        /// returns the status of the operation
+        /// </returns>
+        public bool CreditAmount(decimal amount)
+        {
+            //this.currentBalance = this.currentBalance + amount;
+            this.currentBalance += amount;
+            return true;
+        }
+        /// <summary>
+        /// Method to debit amount from account
+        /// </summary>
+        /// <param name="amount">
+        /// amount to be debited from account
+        /// </param>
+        /// <returns>
+        /// returns the status of the operation
+        /// </returns>
+        public bool DebitAmount(decimal amount)
+        {
+            if (amount > 0)
+            {
+                decimal temp = this.currentBalance - amount;
+                if (temp >= MINIMUMBALANCE)
+                {
+                    this.currentBalance -= amount;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        #endregion
+    }
+}
---
+++ day-4/OTS/OTS.UserInterface/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OTS.Entities;
+
+namespace OTS.UserInterface
+{
+    /// <summary>
+    /// User interface class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// method to create an account object on the basis of values taken from user
+        /// </summary>
+        /// <returns>
+        /// returns the reference of created account object
+        /// </returns>
+        static Account CreateAccount()
+        {
+            Account account = new Account();
+
+            Console.Write("\nName: ");
+            account.AccountHolderName = Console.ReadLine();
+
+            Console.Write("Id: ");
+            account.AccountId = int.Parse(Console.ReadLine());
+
+            Console.Write("Balance: ");
+            account.CurrentBalance = decimal.Parse(Console.ReadLine());
+
+            return account;
+        }
+        static decimal GetAmount()
+        {
+            Console.Write("\nEnter amount to transfer: ");
+            return decimal.Parse(Console.ReadLine());
+        }
+        static void Main()
+        {
+            Account debitAccount = CreateAccount();
+            Account creditAccount = CreateAccount();
+
+            decimal amountToTransfer = GetAmount();
+
+            TransferAmount(debitAccount, creditAccount, amountToTransfer);
+
+            DisplayBalance(creditAccount);
+            DisplayBalance(debitAccount);
+        }
+        /// <summary>
+        /// Method to display balance
+        /// </summary>
+        /// <param name="account">
+        /// account whose balance should be displayed
+        /// </param>
+        private static void DisplayBalance(Account account)
+        {
+            Console.WriteLine($"{account.AccountHolderName} has {account.CurrentBalance} as balance");
+        }
+
+        /// <summary>
+        /// method to transfer amount from one account to another
+        /// </summary>
+        /// <param name="debitAccount">account where amount should be debited from</param>
+        /// <param name="creditAccount">account where amount should be credited to</param>
+        /// <param name="amountToTransfer">
+        /// amount to transfer
+        /// </param>
+        private static void TransferAmount(Account debitAccount, Account creditAccount, decimal amountToTransfer)
+        {
+            bool debitStatus = debitAccount.DebitAmount(amountToTransfer);
+            if (debitStatus)
+            {
+                bool creditStatus = creditAccount.CreditAmount(amountToTransfer);
+                if (!creditStatus)
+                    debitAccount.CreditAmount(amountToTransfer);
+            }
+        }
+    }
+}
