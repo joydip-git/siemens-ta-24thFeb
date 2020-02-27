1ecddea693db8e34e82f82f433a1117ae5ed2f1e
siemens-c2c <joy_mon78@yahoo.co.in>
2020-02-27 11:04:39 +0530

    Created EMS App

---
+++ day-4/EMS/EMS.Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Entities
+{
+    public class Employee
+    {
+        //data members
+        private string employeeName;
+        private int employeeId;
+        private decimal basicPayment;
+        private decimal daPayment;
+        private decimal hraPayment;
+        //private decimal totalSalary;
+
+        public void Assign(string empName, int empId, decimal basicPay, decimal daPay, decimal hraPay)
+        {
+            employeeName = empName;
+            employeeId = empId;
+            basicPayment = basicPay;
+            daPayment = daPay;
+            hraPayment = hraPay;
+        }
+
+        public decimal CalculateSalary()
+        {
+            decimal total = basicPayment + daPayment + hraPayment;
+            return total;
+        }
+    }
+}
---
+++ day-4/EMS/EMS.UserInterface/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EMS.Entities;
+
+namespace EMS.UserInterface
+{
+    class Program
+    {
+        static Employee AcceptValuesandCreateObject()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Basic Payment: ");
+            decimal basic = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Da Payment: ");
+            decimal da = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Hra Payment: ");
+            decimal hra = decimal.Parse(Console.ReadLine());
+
+            Employee anilObjectRef = new Employee();
+            anilObjectRef.Assign(empId: id, empName: name, hraPay: hra, daPay: da, basicPay: basic);           
+            return anilObjectRef;
+        }
+        static void Main()
+        {
+            Employee anilRef = AcceptValuesandCreateObject();
+            PrintSalary(anilRef);
+            //string str = "siemens";
+
+
+            //Employee sunilObjRef = new Employee();
+            //sunilObjRef.CalculateSalary();
+        }
+        static void PrintSalary(Employee anil)
+        {
+            decimal totalSal = anil.CalculateSalary();
+            Console.WriteLine(totalSal);
+        }
+    }
+}
