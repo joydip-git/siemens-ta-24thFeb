b0636f4c7ec895249cdf9c7a1b4acd3c086bcb24
siemens-c2c <joy_mon78@yahoo.co.in>
2020-03-12 10:51:24 +0530

    updated collection and generic collection siwth hashtable, etc.

--- day-9/CollectionsandGenericCollections_Part2/CollectionsandGenericCollections_Part2/Program.cs
+++ day-9/CollectionsandGenericCollections_Part2/CollectionsandGenericCollections_Part2/Program.cs
@@ -45,7 +45,7 @@ namespace CollectionsandGenericCollections_Part2
     }
     class Program
     {
-        static void Main()
+        static void HashDemo()
         {
             HashSet<int> intSet = new HashSet<int>();
 
@@ -72,6 +72,160 @@ namespace CollectionsandGenericCollections_Part2
             {
                 Console.WriteLine(item);
             }
+        }
+        static void StackandStackofTDemo()
+        {
+            //Stack stack = new Stack();
+            //stack.Push(1);
+            //stack.Push(12.34);
+            //stack.Push('a');
+            //stack.Push("siemens");
+            //foreach (object item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("after removal");
+            //stack.Pop();
+            //foreach (object item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(12);
+            stack.Push('a');
+            stack.Push(5);
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            object lastElement = stack.Peek();
+            Console.WriteLine($"last element added: {lastElement}");
+
+            Console.WriteLine("after removal");
+            stack.Pop();
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            IEnumerator<int> enumerator = stack.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+        static void QueueDemo()
+        {
+            /*
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(12.34);
+            queue.Enqueue('a');
+            queue.Enqueue("siemens");
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("after removal");
+            queue.Dequeue();
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            */
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(34);
+            queue.Enqueue('a');
+            queue.Enqueue(5);
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("after removal");
+            queue.Dequeue();
+            foreach (object item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void HashTableandDictionaryDemo()
+        {
+            //Hashtable hashtable = new Hashtable();
+            //hashtable.Add(1, "siemens");
+            //hashtable.Add('a', 12.34);
+            //hashtable.Add("s", 'f');
+            ////hashtable.Add(1, 34);
+            //hashtable['d'] = 34;
+            //foreach (DictionaryEntry item in hashtable)
+            //{
+            //    Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            //}
+            //hashtable.Remove('d');
+            //Console.WriteLine("after removal");
+            //foreach (DictionaryEntry item in hashtable)
+            //{
+            //    Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            //}
+            Dictionary<int, char> keyValuePairs = new Dictionary<int, char>();
+            keyValuePairs.Add(1, 'a');
+            keyValuePairs.Add(0, 'd');
+            keyValuePairs.Add(3, 'b');
+            //hashtable.Add(1, 34);
+            keyValuePairs['d'] = 'c';
+            foreach (KeyValuePair<int, char> item in keyValuePairs)
+            {
+                Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            }
+            keyValuePairs.Remove('d');
+            Console.WriteLine("after removal");
+            foreach (KeyValuePair<int, char> item in keyValuePairs)
+            {
+                Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            }
+        }
+        static void SortedListDemo()
+        {
+            /*
+            SortedList sortedList = new SortedList();
+            sortedList.Add(2, 'f');
+            sortedList.Add(1, "siemens");
+            sortedList.Add(4, 12.34);
+            //hashtable.Add(1, 34);
+            sortedList[3] = 34;
+            foreach (DictionaryEntry item in sortedList)
+            {
+                Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            }
+            sortedList.Remove(3);
+            Console.WriteLine("after removal");
+            foreach (DictionaryEntry item in sortedList)
+            {
+                Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            }*/
+            SortedList<int,object> sortedList = new SortedList<int,object>();
+            sortedList.Add(2, 'f');
+            sortedList.Add(1, "siemens");
+            sortedList.Add(4, 12.34);
+            //hashtable.Add(1, 34);
+            sortedList[3] = 34;
+            foreach (KeyValuePair<int,object> item in sortedList)
+            {
+                Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            }
+            sortedList.Remove(3);
+            Console.WriteLine("after removal");
+            foreach (KeyValuePair<int,object> item in sortedList)
+            {
+                Console.WriteLine($"key:{item.Key}, value:{item.Value}");
+            }
+        }
+        static void Main()
+        {
+            SortedListDemo();
+            //HashTableandDictionaryDemo();
+            //StackandStackofTDemo();
+            //QueueDemo();
         }
     }
 }
