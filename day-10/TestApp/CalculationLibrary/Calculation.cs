b9ff09c4e3df096f65f8fe5febdcb9e4584b3e29
siemens-c2c <joy_mon78@yahoo.co.in>
2020-03-13 15:45:51 +0530

    created nunit test case application

---
+++ day-10/TestApp/CalculationLibrary/Calculation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationLibrary
+{
+    public class Calculation
+    {
+        //[Obsolete("use the other method...this is deprecated")]
+        //public int Add(int x, int y)
+        //{
+        //    return (x + y);
+        //}
+        public T Add<T>(T x, T y) where T:struct
+        {
+            return default(T);
+        }
+        public int Subtract(int a, int b)
+        {
+            if (a > b)
+                return (a - b);
+            else
+                return b - a;
+        }
+        public int Divide(int a, int b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+
+    //class Check
+    //{
+    //    public void foo()
+    //    {
+    //        Calculation calculation = new Calculation();
+    //        calculation.Add(12, 13);
+    //    }
+    //}
+}
---
+++ day-10/TestApp/CalculationLibraryTest/CalculationTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using CalculationLibrary;
+
+namespace CalculationLibraryTest
+{
+    //[Serializable]
+    //public class Vehicle
+    //{
+    //    public int Id { get; set; }
+    //    public string ChasisNo { get; set; }
+    //    public string ModelName { get; set; }
+    //}
+    [TestFixture]
+    public class CalculationTest
+    {
+        private Calculation calculation;
+
+        //this method will be called at the begining of everything and once only
+        [OneTimeSetUp]
+        public void InitializeFirst()
+        {
+            Console.WriteLine("one time initialization");
+        }
+
+        //this method will be called at the end of everything and once only
+        [OneTimeTearDown]
+        public void Dispose()
+        {
+            Console.WriteLine("complete clear");
+        }
+
+        //this method will be called at the begining of every test case
+        [SetUp]
+        public void Initialize()
+        {
+            calculation = new Calculation();
+            Console.WriteLine("obj created");
+        }
+
+        //this method will be called at the end of every test case
+        [TearDown]
+        public void Clear()
+        {
+            calculation = null;
+            Console.WriteLine("obj nullified");
+        }
+        [TestCase]
+        public void Divide_When_Values_Are_Proper()
+        {
+            int actualResult = calculation.Divide(12, 3);
+            //CollectionAssert.AreEqual()
+            Assert.AreEqual(4, actualResult);
+        }
+
+        [TestCase]
+        public void Divide_When_Exception_Occurs()
+        {
+            //TestDelegate test = delegate ()
+            //{
+            //    Calculation calculation = new Calculation();
+            //    calculation.Divide(12, 0);
+            //};
+            TestDelegate test = () =>
+             {
+                 Calculation calculation = new Calculation();
+                 calculation.Divide(12, 0);
+             };
+            Assert.Throws<DivideByZeroException>(test);
+        }
+    }
+}
